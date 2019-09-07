be25216941d132e8d9e9a790b7f21aaea7ab88f1
jwu56 <25115324+jwu56@users.noreply.github.com>
2019-09-08 08:27:42 +1000

    Finalise Weather API requests

---
+++ scripts/script.js
@@ -0,0 +1,51 @@
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(weather);
+  } else {
+    alert("Unfortunately, your browser does not support geolocation.");
+}
+}
+function round(value, decimals) {
+  return Number(Math.round(value+'e'+decimals)+'e-'+decimals);
+}
+function weather(position) {
+  var weatherkey = '26b934435cecd3fbd99d8c3ead2bf36b';
+  var today = new Date();
+  var dd = String(today.getDate()).padStart(2, '0');
+  var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+  var yyyy = today.getFullYear();
+  var lat = position.coords.latitude;
+  var long = position.coords.longitude;
+  today = yyyy+mm+dd;
+  jQuery.ajax({
+    url: "https://cors-anywhere.herokuapp.com/https://www.longpaddock.qld.gov.au/cgi-bin/silo/DataDrillDataset.php",
+    dataType: "JSON",
+    data: {
+      start: today,
+      finish: today,
+      lat: round(lat,6),
+      lon: round(long,6),
+      format: "JSON",
+      username: "example@email.com",
+      password: "password",
+      comment: "R"
+    },
+    success: function(result) {
+      v = result.data[0].variables[0].value
+      $('#rainfall').html("The rainfall for this location today is: " + v + "mm");
+    }
+  });
+  jQuery.ajax({
+    url: "https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather",
+    dataType: "JSON",
+    data: {
+      lat: round(lat,6),
+      lon: round(long,6),
+      appid: weatherkey
+    },
+    success: function(result) {
+      console.log(result);
+    }
+  });
+}
+onload = getLocation;
