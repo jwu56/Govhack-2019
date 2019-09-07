bb52a0f757da22b99990aabb9e0c4b70f1330a6d
jwu56 <25115324+jwu56@users.noreply.github.com>
2019-09-07 18:09:36 +1000

    Initial Commit

---
+++ scripts/script.js
@@ -0,0 +1,56 @@
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(weather);
+  } else {
+    alert("Unfortunately, your browser does not support geolocation.");
+}
+}
+function round(value, decimals) {
+  return Number(Math.round(value+'e'+decimals)+'e-'+decimals);
+}
+/*
+function pos(lat1,lon1) {
+  var R = 6371e3; // Earth's radius in metres
+  var φ1 = lat1.toRadians();
+  var φ2 = lat2.toRadians();
+  var Δφ = (lat2-lat1).toRadians();
+  var Δλ = (lon2-lon1).toRadians();
+
+  var a = Math.sin(Δφ/2) * Math.sin(Δφ/2) +
+          Math.cos(φ1) * Math.cos(φ2) *
+          Math.sin(Δλ/2) * Math.sin(Δλ/2);
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+
+  var d = R * c;
+}
+*/
+function weather(position) {
+  var today = new Date();
+  var dd = String(today.getDate()).padStart(2, '0');
+  var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+  var yyyy = today.getFullYear();
+  var lat = position.coords.latitude;
+  var long = position.coords.longitude;
+  today = dd+mm+yyyy;
+  jQuery.ajax({
+    /*
+    beforeSend: function(xhr){xhr.setRequestHeader('Origin', 'longpaddock.qld.gov.au');},
+    */
+    url: "https://cors-anywhere.herokuapp.com/www.longpaddock.qld.gov.au/cgi-bin/silo/DataDrillDataset.php",
+    dataType: "JSON",
+    data: {
+      start: today,
+      finish: today,
+      lat: round(lat,6),
+      lon: round(long,6),
+      format: "JSON",
+      username: "example@email.com",
+      password: "password"
+    },
+    success: function(result) {
+      console.log(result);
+      $( "#weather-temp" ).html( result );
+    }
+  });
+}
+onload = getLocation;
