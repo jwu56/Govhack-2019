45f5a9689ffe12d6df25517d76bb23585e8c8d87
jwu56 <25115324+jwu56@users.noreply.github.com>
2019-09-07 20:32:02 +1000

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -8,22 +8,6 @@ function getLocation() {
 function round(value, decimals) {
   return Number(Math.round(value+'e'+decimals)+'e-'+decimals);
 }
-/*
-function pos(lat1,lon1) {
-  var R = 6371e3; // Earth's radius in metres
-  var φ1 = lat1.toRadians();
-  var φ2 = lat2.toRadians();
-  var Δφ = (lat2-lat1).toRadians();
-  var Δλ = (lon2-lon1).toRadians();
-
-  var a = Math.sin(Δφ/2) * Math.sin(Δφ/2) +
-          Math.cos(φ1) * Math.cos(φ2) *
-          Math.sin(Δλ/2) * Math.sin(Δλ/2);
-  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-
-  var d = R * c;
-}
-*/
 function weather(position) {
   var today = new Date();
   var dd = String(today.getDate()).padStart(2, '0');
